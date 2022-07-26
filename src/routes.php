9835129a72372e86a344fd175b5d995f5c0b71a2
islamrumon <islamrumon420@gmail.com>
2022-07-26 08:26:05 +0600

    package update

---
+++ src/routes.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Config;
+
+Route::post('comments', [Config::get('comments.controller')::class,'store'] )->name('comments.store');
+Route::delete('comments/{comment}', [Config::get('comments.controller')::class,'destroy'])->name('comments.destroy');
+Route::put('comments/{comment}', [Config::get('comments.controller')::class,'update'])->name('comments.update');
+Route::post('comments/{comment}', [Config::get('comments.controller')::class,'reply'])->name('comments.reply');
